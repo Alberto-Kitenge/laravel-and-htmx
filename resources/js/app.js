8c5de37d860af24ef2c143c8b64f9b0478a73d51
Alberto Kitenge <kitengewalberto@gmail.com>
2025-11-11 17:10:02 +0100

    starter project

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import "./bootstrap";
+
+import "htmx.org";
+
+/*
+// Configuration CSRF pour Laravel
+document.addEventListener("htmx:configRequest", (event) => {
+    event.detail.headers["X-CSRF-TOKEN"] = document.querySelector(
+        'meta[name="csrf-token"]'
+    ).content;
+});
+*/
